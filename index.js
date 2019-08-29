a098762402b079f0d36cb5aab221405cbeafdf56
tkp75 <tommi.pakarinen@gmail.com>
2019-08-29 22:15:10 +0300

    3.4: DELETE /api/persons/:id endpoint added, error message improved

--- index.js
+++ index.js
@@ -27,6 +27,23 @@ const data = {
   ]
 }
 
+const errMsg = (operation, endpoint) => `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <meta charset="utf-8">
+    <title>Error</title>
+    </head>
+    <body>
+    <pre>Cannot ${operation} ${endpoint}</pre>
+    </body>
+    </html>
+    `
+
+/*
+    GET
+*/
+
 app.get('/', (req, res) => {
   res.send(`<h1>Puhelinluettelo Backend!</h1>
   <p>Service is running at <a href="http://localhost:${PORT}${API_BASE}">${API_BASE}</a></p>`)
@@ -36,20 +53,40 @@ app.get('/info', (req, res) => {
   res.send(`Phonebook has ${data.persons.length} people<br>${new Date()}`)
 })
 
+app.get(API_BASE, (req, res) => {
+  res.json(data)
+})
+
 app.get(API_BASE+"/:id", (req, res) => {
   const id = Number(req.params.id)
   const person = data.persons.find(p => p.id === id)
   if (person) {
     res.json(person)
   } else {
-    res.status(404).send(`ERROR: person.id == ${id} did not match with anyone`)
+    res.status(404).send(errMsg(req.method, req.originalUrl))
   }
 })
 
-app.get(API_BASE, (req, res) => {
-  res.json(data)
+/*
+    DELETE
+*/
+
+app.delete(API_BASE+"/:id", (req, res) => {
+  console.log('DELETE: data.persons=',data.persons)
+  const id = Number(req.params.id)
+  console.log('DELETE: id=',id)
+  const person = data.persons.find(p => p.id === id)
+  if (person) {
+    data.persons = data.persons.filter(p => p.id != person.id)
+    res.status(204).end()
+  } else {
+    res.status(404).send(errMsg(req.method, req.originalUrl))
+  }
 })
 
+/*
+    LISTEN
+*/
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
