19e82549d782f9e85e6c61478449c5c51b659c7d
tkp75 <tommi.pakarinen@gmail.com>
2019-08-29 23:16:52 +0300

    3.6: error handling added for post

--- index.js
+++ index.js
@@ -90,11 +90,19 @@ app.delete(API_BASE+"/:id", (req, res) => {
 */
 
 app.post(API_BASE, (req, res) => {
-  const person = req.body
-  console.log(person)
-  person.id=Math.random() * Number.MAX_SAFE_INTEGER
-  data.persons.push(person)
-  res.json(person)
+  console.log("POST: body=",req.body)
+  console.log("POST: name=",req.body.name)
+  console.log("POST: number=",req.body.number)
+  console.log("POST: id=",req.body.id)
+  if (req.body.name && req.body.number
+    && !data.persons.find(p => p.name === req.body.name)) {
+    const person = { "name": req.body.name, "number": req.body.number }
+    person.id=Math.floor(Math.random() * Number.MAX_SAFE_INTEGER)
+    data.persons.push(person)
+    res.json(person)
+  } else {
+    res.status(404).send(errMsg(req.method, req.originalUrl))
+  }
 })
 
 
