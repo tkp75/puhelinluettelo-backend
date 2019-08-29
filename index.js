6fd65a052e9b3ecfa69c33bc7ec3515c84acd3a6
tkp75 <tommi.pakarinen@gmail.com>
2019-08-29 21:08:10 +0300

    3.3: /api/persons/:id endpoint added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const PORT = 3001
 const API_BASE = "/api/persons"
-const persons = {
+const data = {
   "persons":[
     { 
       "name": "Arto Hellas", 
@@ -27,20 +27,29 @@ const persons = {
   ]
 }
 
+app.get('/', (req, res) => {
+  res.send(`<h1>Puhelinluettelo Backend!</h1>
+  <p>Service is running at <a href="http://localhost:${PORT}${API_BASE}">${API_BASE}</a></p>`)
+})
 
-  app.get('/', (req, res) => {
-    res.send(`<h1>Puhelinluettelo Backend!</h1>
-    <p>Service is running at <a href="http://localhost:${PORT}${API_BASE}">${API_BASE}</a></p>`)
-  })
-  
-  app.get('/info', (req, res) => {
-    res.send(`Phonebook has ${persons.length} people<br>${new Date()}`)
-  })
+app.get('/info', (req, res) => {
+  res.send(`Phonebook has ${data.persons.length} people<br>${new Date()}`)
+})
 
-  app.get(API_BASE, (req, res) => {
-    res.json(persons)
-  })
+app.get(API_BASE+"/:id", (req, res) => {
+  const id = Number(req.params.id)
+  const person = data.persons.find(p => p.id === id)
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).send(`ERROR: person.id == ${id} did not match with anyone`)
+  }
+})
 
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-  })
+app.get(API_BASE, (req, res) => {
+  res.json(data)
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
