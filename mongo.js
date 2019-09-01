b910325de3addfb1fefa4372e7d4bee664d29380
tkp75 <tommi.pakarinen@gmail.com>
2019-09-01 20:41:00 +0300

    3.12: mongo.js list and save persons

--- mongo.js
+++ mongo.js
@@ -1,46 +1,87 @@
 const mongoose = require('mongoose')
 
-if ( process.argv.length<3 ) {
-  console.log('give password as argument')
+// Check command line arguments
+if (process.argv.length < 3 || process.argv.length > 5) {
+  console.log(`ERROR: incorrect invocation, exiting.\n`
+    +`To list all persons on phonebook:\n`
+    +`\tnode mongo.js <password>\n`
+    +`To list a person with known id:\n`
+    +`\tnode mongo.js <password> <id>\n`
+    +`To add a person to phonebook:\n`
+    +`\tnode mongo.js <password> <name> <phone number>`)
   process.exit(1)
 }
 
+// FUNCTIONS
+// - Print all persons
+const getAll = () => {
+  console.log('phonebook:')
+  Person.find({})
+    .then(result => {
+      result.forEach(p => {
+        console.log(`${p.name} ${p.number}`)
+      })
+      mongoose.connection.close()
+    })
+    .catch(e => {
+      console.log(`ERROR:getAll:`,e)
+      process.exit(1)
+    })
+}
+// - Print person with id
+const getOne = (id) => {
+  console.log('person:')
+  Person.find({id: {$eq: id}})
+    .then(result => {
+      result.forEach(p => {
+        console.log(`${p.name} ${p.number}`)
+      })
+      mongoose.connection.close()
+    })
+    .catch(e => {
+      console.log(`ERROR:getOne:`,e)
+      process.exit(1)
+    })
+}
+// - Save person into phonebook
+const saveOne = (name, number, id) => {
+  const person = new Person({
+    name: name,
+    number: number,
+    id: id,
+  })
+  person.save()
+    .then(response => {
+      console.log(`added ${name} number ${number} to phonebook`)
+      mongoose.connection.close()
+    })
+    .catch(e => {
+      console.log(`ERROR:saveOne:`,e)
+      process.exit(1)
+    })
+}
+
+// MAIN
+// - Connect and create Mongoose model
 const password = process.argv[2]
-
-const url =
-  `mongodb+srv://puhelinluettelo:${password}@fsdev-ctcbm.mongodb.net/test?retryWrites=true&w=majority`
-
+const url = `mongodb+srv://puhelinluettelo:${password}@fsdev-ctcbm.mongodb.net/test?retryWrites=true&w=majority`
 mongoose.connect(url, { useNewUrlParser: true })
-
 const personSchema = new mongoose.Schema({
   name: String,
   number: String,
   id: Number,
 })
-
 const Person = mongoose.model('Person', personSchema)
-/*
-const person = new Person({
-  name: 'Aku Ankka',
-  number: '000-313',
-  id: 1,
-})
-*/
-/*
-const person = new Person({
-  name: 'Iines Ankka',
-  number: '000-123',
-  id: 2,
-})
 
-person.save().then(response => {
-  console.log('person saved!');
-  mongoose.connection.close();
-})
-*/
-Person.find({id: {$eq: 1}}).then(result => {
-  result.forEach(p => {
-    console.log(p)
-  })
-  mongoose.connection.close()
-})
+// - select function
+switch(process.argv.length) {
+  case 3:
+    getAll()
+    break
+  case 4: // (id)
+    getOne(process.argv[3])
+    break
+  case 5: // (name, phone number, id)
+    saveOne(process.argv[3],process.argv[4], Math.floor(Math.random() * Number.MAX_SAFE_INTEGER))
+    break
+}
