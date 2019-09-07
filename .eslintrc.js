f2c71242feba52bb8a739dcd1b834358b330bf32
tkp75 <tommi.pakarinen@gmail.com>
2019-09-07 19:44:14 +0300

    3.22: updated linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,6 +29,20 @@ module.exports = {
         'semi': [
             'error',
             'never'
-        ]
+        ],
+        'eqeqeq': 'error',
+        'no-trailing-spaces': 'error',
+        'object-curly-spacing': [
+            'error',
+            'always'
+        ],
+        'arrow-spacing': [
+            'error',
+            {
+                'before': true,
+                'after': true
+	    }
+        ],
+        'no-console': 0
     }
 }
